d117af6227a8ff4ef701a8e095a78142cc423676
jw406 <jwvic.2357@gmail.com>
2021-07-22 20:47:04 -0400

    use lombok to replace boilerplates; remove commented out code in springmvc.xml

--- src/main/java/org/Foo/Bar/Entities/User.java
+++ src/main/java/org/Foo/Bar/Entities/User.java
@@ -13,11 +13,18 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 import org.springframework.stereotype.Component;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
 @Component
+@Getter
+@Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Table(name = "proj02_User")
 public class User {
+  @Setter(value = AccessLevel.NONE)
   @JsonIgnore
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PROJ02_USER")
@@ -31,32 +38,4 @@ public class User {
   private String name;
   @Column
   private Long pokeToken;
-
-  public Long getPokeToken() {
-    return pokeToken;
-  }
-
-  public void setPokeToken(Long pokeToken) {
-    this.pokeToken = pokeToken;
-  }
-
-  public Integer getId() {
-    return id;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
 }
--- src/main/java/org/Foo/Bar/RestObjects/CheckoutModel.java
+++ src/main/java/org/Foo/Bar/RestObjects/CheckoutModel.java
@@ -1,31 +1,12 @@
 package org.Foo.Bar.RestObjects;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class CheckoutModel {
   private String itemName;
   private Long quantities;
   private Long amount;
-
-  public String getItemName() {
-    return itemName;
-  }
-
-  public void setItemName(String itemName) {
-    this.itemName = itemName;
-  }
-
-  public Long getQuantities() {
-    return quantities;
-  }
-
-  public void setQuantities(Long quantities) {
-    this.quantities = quantities;
-  }
-
-  public Long getAmount() {
-    return amount;
-  }
-
-  public void setAmount(Long amount) {
-    this.amount = amount;
-  }
 }
